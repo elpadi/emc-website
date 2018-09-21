f24681ffa6ee18e06073aa761766e91de92d6cae
Carlos Padilla <padi_05@yahoo.com>
2018-09-21 15:59:47 -0400

    URL hash change on video tag filter

--- wp-content/themes/emc-kingsize-child/js/listing.js
+++ wp-content/themes/emc-kingsize-child/js/listing.js
@@ -3,6 +3,7 @@
  * from Vimeo.
  */
 var EMC_Listing = (function($) {
+	var TAGS_HASH_DELIMETER = '_';
 	var createTagButton = function(name) {
 		let b = document.createElement('button');
 		b.className = 'radio plain';
@@ -11,36 +12,77 @@ var EMC_Listing = (function($) {
 	};
 	var setup = function(container) {
 		let f = container.querySelector('form');
-		getListingTags(container).map(createTagButton).forEach(b => f.appendChild(b));
-		f.addEventListener('click', function(e) {
-			if (e.target.nodeName === 'BUTTON') {
+		var allTags = {};
+		container.selectedTags = {};
+		container.allTags = {};
+		getListingItems(container).forEach(function(item) {
+			// put each item tags in an object
+			item.tags = {};
+			JSON.parse(item.dataset.tags).forEach(function(t) {
+				item.tags[t] = true;
+				allTags[t] = true;
+			});
+		});
+		Object.keys(allTags).sort().forEach(function(t) {
+			container.allTags[t] = true;
+		});
+		$(container).find('form')
+			.append($(getListingTags(container).map(createTagButton)))
+			.on('click', 'button', function(e) {
+				var b = this;
 				e.preventDefault();
-				e.target.classList.toggle('selected');
-				filterByTags(container, getEnabledTags(container));
-			}
+				b.classList.toggle('selected');
+				if (b.innerHTML in container.selectedTags) {
+					delete container.selectedTags[b.innerHTML];
+				}
+				else {
+					container.selectedTags[b.innerHTML] = true;
+				}
+				updateLocationHash(getEnabledTags(container));
 		});
 		f.addEventListener('submit', function(e) {
 			e.preventDefault();
 		});
+		window.addEventListener("hashchange", function() { onHashChange(container); });
+		onHashChange(container);
 	};
 	var getListingItems = function(container) {
 		return Array.from(container.getElementsByTagName('article'));
 	};
 	var getListingTags = function(container) {
-		let tags = [].concat.apply([], getListingItems(container).map(n => JSON.parse(n.dataset.tags)));
-		tags.sort();
-		return tags.filter((t, i, all) => all.indexOf(t) == i);
+		return Object.keys(container.allTags);
 	};
 	var getTagButtons = function(container) {
 		return Array.from(container.querySelectorAll('form button'));
 	};
 	var getEnabledTags = function(container) {
-		return getTagButtons(container).filter(n => n.classList.contains('selected')).map(n => n.innerHTML);
+		return Object.keys(container.selectedTags);
 	};
 	var clearTags = function(container) {
 		return getTagButtons(container).forEach(n => n.classList.remove('selected'));
 	};
-	var filterByTags = function(container, tags) {
+	var updateLocationHash = function(tags) {
+		if (tags.length > 0) {
+			location.hash = '#' + encodeURIComponent(tags.join(TAGS_HASH_DELIMETER));
+		}
+		else location.hash = '';
+	};
+	var onHashChange = function(container) {
+		console.log('EMC_Listing.onHashChange', location.hash);
+		container.selectedTags = {};
+		if (location.hash != '') {
+			var tags = decodeURIComponent(location.hash.substr(1)).split(TAGS_HASH_DELIMETER);
+			console.log('EMC_Listing.onHashChange', tags);
+			tags.map(function(t) {
+				if (t in container.allTags) {
+					container.selectedTags[t] = true;
+				}
+			});
+		}
+		updateTagFilter(container, getEnabledTags(container));
+	};
+	var updateTagFilter = function(container, tags) {
+		console.log('EMC_Listing.updateTagFilter', tags);
 		getListingItems(container).forEach(function(n) {
 			if (tags.length == 0) return n.classList.remove('disabled');
 			let tagsInItem = tags.filter(t => JSON.parse(n.dataset.tags).includes(t));
